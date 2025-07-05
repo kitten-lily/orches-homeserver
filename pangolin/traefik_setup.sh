0f5851b2806fbd83c2fe6eb44de8ba57d3d74e70
Lily <20535390+kitten-lily@users.noreply.github.com>
2025-07-05 18:33:58 +0200

    Remove pangolin

--- pangolin/traefik_setup.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-. /var/lib/orches/repo/scripts/utils.sh
-
-podman_secret Pangolin "Cloudflare Token" pangolin-cloudflare-token
-podman_secret Pangolin "Domain Name" pangolin-domain-name
