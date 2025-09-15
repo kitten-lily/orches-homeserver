935c05db656cd6438b4152c74d7a944f21ddc5aa
Lily <20535390+kitten-lily@users.noreply.github.com>
2025-09-15 09:58:34 +0000

    Use secrets from 1Password

--- ente/ente-setup.sh
+++ ente/ente-setup.sh
@@ -10,19 +10,8 @@ if [ ! -d "${CONFIG_PATH}" ]; then
     mkdir -p "${CONFIG_PATH}"
 fi
 
-rm "${CONFIG_FILE}"
 if [ ! -f "${CONFIG_FILE}" ]; then
     $OP_PODMAN --user root --volume $TEMPLATE_PATH:/template.yml:z \
     --volume $CONFIG_PATH:/config:z \
     $OP_IMAGE op inject --in-file /template.yml  --out-file /config/museum.yaml --force
-
-    cat <<EOF >>museum.yaml
-
-key:
-      encryption: $(head -c 32 /dev/urandom | base64 | tr -d '\n')
-      hash: $(head -c 64 /dev/urandom | base64 | tr -d '\n')
-
-jwt:
-      secret: $(head -c 32 /dev/urandom | base64 | tr -d '\n' | tr '+/' '-_')
-EOF
 fi
