b161921d9379c97c46724fbb34599b619d019cad
Lily <20535390+kitten-lily@users.noreply.github.com>
2025-09-15 09:44:05 +0000

    Add Ente config generation

---
+++ ente/ente-setup.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+. /var/lib/orches/repo/scripts/utils.sh
+
+TEMPLATE_PATH="/var/lib/orches/repo/ente/templates/museum.yaml"
+CONFIG_PATH="/var/lib/orches/repo/ente/config"
+CONFIG_FILE="${CONFIG_PATH}/museum.yaml"
+
+if [ ! -d "${CONFIG_PATH}" ]; then
+    mkdir -p "${CONFIG_PATH}"
+fi
+
+if [ ! -f "${CONFIG_FILE}" ]; then
+    $OP_PODMAN --user root --volume $TEMPLATE_PATH:/template.yml:z \
+    --volume $CONFIG_PATH:/config:z \
+    $OP_IMAGE op inject --in-file /template.yml  --out-file /config/museum.yml --force
+fi
