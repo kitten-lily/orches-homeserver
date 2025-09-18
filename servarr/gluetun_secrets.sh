cc0a87e8153266681d355e04deb4a90afa129c39
Lily <20535390+kitten-lily@users.noreply.github.com>
2025-09-18 07:56:41 +0000

    Add secret for countries

--- servarr/gluetun_secrets.sh
+++ servarr/gluetun_secrets.sh
@@ -3,3 +3,4 @@
 . /var/lib/orches/repo/scripts/utils.sh
 
 podman_secret Servarr "Wireguard Private Key" servarr-wireguard-private-key
+podman_secret Servarr "Wireguard Countries" servarr-wireguard-countries
